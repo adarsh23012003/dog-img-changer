1cfe9512602159e5996df6ff232db25a49b719b9
Adarsh <adarshmaurya2303@gmail.com>
2022-01-21 11:25:06 +0530

    progect insilice

---
+++ src/views/Contact.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+      {{message}}
+  </div>
+</template>
+
+<script>
+export default {
+data(){
+    return{
+        message:"This is Contact Page"
+    }
+}
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <img src="../assets/logo.png" alt="">
+    <h2>Dog Name</h2>
+    <button class="btn" @click="fetchDogs">Fetch Dog Image</button>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: "Home",
+  data(){
+    return{
+    count : 5
+
+    }
+  }, 
+
+  methods:{
+      fetchDogs(){
+        this.count = this.count + 1
+        console.log(this.count)
+      }
+  }
+};
+</script>
+
+<style scoped>
+.btn{
+  height: 25px;
+  width: 150px;
+  background-color: aquamarine;
+  text-align: center;
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: 15px;
+  border-style : solid 
+
+}
+</style>>
+
